f727be7e020312a7aba64227004827d47b8ca0ba
viktor <viktor.ermilov.96@mail.ru>
2024-10-04 12:51:35 +0300

    делаю нагрузочное тестирование

---
+++ locustfile.py
@@ -0,0 +1,17 @@
+from locust import HttpUser, task, between
+import random
+
+class ChatUser(HttpUser):
+    wait_time = between(1, 2)
+
+    @task
+    def chat(self):
+        questions = [
+            "Как тебя зовут?",
+            "Что ты умеешь?",
+            "Как дела?",
+            "Привет",
+            "Как найти информацию о продукте?"
+        ]
+        question = random.choice(questions)
+        self.client.post("/chat", json={"question": question})
